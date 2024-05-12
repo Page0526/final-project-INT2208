bc3bad729d19d347b1cd8713f60c83e761192f86
Page0526 <phuongtrang2652004@gmail.com>
2024-05-12 15:27:30 +0700

    improve frontend

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -2,15 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
 
-# Create your models here.
-<<<<<<< HEAD
-class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    username = models.TextField()
-    profile_image = models.ImageField(upload_to='avatars/', blank=True) # save avatar
-=======
->>>>>>> bbd66392acc56c97bdab12558c1bb7bf6521ca11
-    
+# Create your models here.    
 # model to save PDF history
 class PDFHistory(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='pdf_histories')
